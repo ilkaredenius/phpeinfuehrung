484351e6621fe0505152f542928ab052e22b616a
I.Gartsev <code@zentralweb.de>
2023-02-21 12:21:31 +0100

    view added

--- minitrm/index.php
+++ minitrm/index.php
@@ -23,10 +23,20 @@ spl_autoload_register(function ($class)
     }
 });
 
+$controllerName = ucfirst($_GET['controller']);
+
+$actionName = ucfirst($_GET['action']);
+
 $controllerClassName = '\\MyApp\\Controller\\'.ucfirst($_GET['controller']).'Controller';
 
 $actionMethodName = ucfirst($_GET['action']).'Action'; 
  
 $controller = new $controllerClassName();
+
+$view = new \MyApp\lib\View(__DIR__.DIRECTORY_SEPARATOR.'view',  $actionName, $controllerName);
+
+$controller->setView($view);
  
 $controller->$actionMethodName();
+
+$view->render();
